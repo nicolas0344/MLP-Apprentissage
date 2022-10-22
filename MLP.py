f3aa83a4b9f6bc315f1371fbb231d6deb74cc0e9
nicolas0344 <nicolas.pralon@etu.umontpellier.fr>
2022-10-22 22:18:40 +0200

    code et rapport

--- MLP.py
+++ MLP.py
@@ -11,142 +11,105 @@ import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import RepeatedKFold, cross_val_score
 from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
 
 
 #Question 1
-
 # code pour generer le graphique XOR
-
-X1 = np.array([0, 1, 0, 1])
-X2 = np.array([0, 0, 1, 1])
+x = np.array([0, 1, 0, 1])
+y = np.array([0, 0, 1, 1])
 groupXOR = np.array([0, 1, 1, 0])
 cdict = {0: 'red', 1: 'green'}
 n = ['(0,1)', '(1,1)', '(0,0)', '(1,0)']
 loc = [(0.0275,0.98), (0.875,0.98), (0.0275,-0.015), (0.875,-0.015)]
-fig, ax = plt.subplots()
 
+fig, ax = plt.subplots()
 for g in np.unique(groupXOR):
     ix = np.where(groupXOR == g)
-    ax.scatter(X1[ix], X2[ix], c = cdict[g], label = g, s=170)
-
+    ax.scatter(x[ix], y[ix], c = cdict[g], label = g, s=170)
 for i, txt, l in zip(range(4), n, loc):
-    ax.annotate(txt, xy=(X1[i], X2[i]), xytext=l, fontsize = 13)
-
+    ax.annotate(txt, xy=(x[i], y[i]), xytext=l, fontsize = 13)
 ax.legend(loc = 'center right')
 plt.title('XOR (OU exclusif)')
 plt.show()
 
- 
-
 # code pour generer le graphique AND
-
 groupAND = np.array([0, 0, 0, 1])
 fig, ax = plt.subplots()
-
 for g in np.unique(groupAND):
     ix = np.where(groupAND == g)
-    ax.scatter(X1[ix], X2[ix], c = cdict[g], label = g, s=170)
-
+    ax.scatter(x[ix], y[ix], c = cdict[g], label = g, s=170)
 for i, txt, l in zip(range(4), n, loc):
-    ax.annotate(txt, xy=(X1[i], X2[i]), xytext=l, fontsize = 13)
-
+    ax.annotate(txt, xy=(x[i], y[i]), xytext=l, fontsize = 13)
 ax.legend(loc = 'center right')
 plt.title('AND (ET)')
 plt.show()
- 
 
 # code pour generer le graphique OR
-
 groupOR = np.array([0, 1, 1, 1])
 fig, ax = plt.subplots()
-
 for g in np.unique(groupOR):
     ix = np.where(groupOR == g)
-    ax.scatter(X1[ix], X2[ix], c = cdict[g], label = g, s=170)
-    
+    ax.scatter(x[ix], y[ix], c = cdict[g], label = g, s=170)
 for i, txt, l in zip(range(4), n, loc):
-    ax.annotate(txt, xy=(X1[i], X2[i]), xytext=l, fontsize = 13)
-    
+    ax.annotate(txt, xy=(x[i], y[i]), xytext=l, fontsize = 13)
 ax.legend(loc = 'center right')
 plt.title('OR (OU inclusif)')
 plt.show()
 
-
-classifier = MLPClassifier(hidden_layer_sizes=(),activation='identity',
-                           solver='lbfgs')
-#Question 2 
-
+#Question 2
 xtrainAND = [[0., 0.], [0., 1.], [1., 0.],[1., 1.]]
 ytrainAND = [0, 0, 0, 1]
-
-classifier.fit(xtrainAND, ytrainAND)
-
-X_test_AND= [[1., 1.],[0.,0.],[1.,1.],[0.,1.]]
-Y_test_AND = [1.,0.,1.,0.]
-Y_predict_AND = classifier.predict(X_test_AND)
-
-#Verification des résultats du classifieur
-np.array(Y_test_AND) - Y_predict_AND
-classifier.score(X_test_AND,Y_test_AND)
-
+xtestAND = [[1., 0.], [1., 1.], [0., 1.],[0., 0.]]
+ytestAND = [0, 1, 0, 0]
+clf = MLPClassifier(hidden_layer_sizes=(), activation='identity',
+                    solver='lbfgs').fit(xtrainAND, ytrainAND)
+print("Score obtenu sur les données de test pour l'apprentissage de l'operator AND: ",
+      clf.score(xtestAND, ytestAND))
 
 #Question 3
 xtrainOR = [[0., 0.], [0., 1.], [1., 0.],[1., 1.]]
 ytrainOR = [0, 1, 1, 1]
+xtestOR = [[1., 1.], [1., 1.], [0., 1.],[0., 0.]]
+ytestOR = [1, 1, 1, 0]
+clf = MLPClassifier(hidden_layer_sizes=(), activation='identity',
+                    solver='lbfgs').fit(xtrainOR, ytrainOR)
+print("Score obtenu sur les données de test pour l'apprentissage de l'operator OR: ", 
+      clf.score(xtestOR, ytestOR))
 
-classifier.fit(xtrainOR, ytrainOR)
-
-X_test_OR = [[1., 1.],[0.,0.],[0.,1.]]
-Y_test_OR = [1.,0.,1.]
-Y_predict_OR = classifier.predict(X_test_OR)
-
-#Verification des résultats du classifieur
-classifier.score(X_test_OR,Y_test_OR)
-
-
-#Question 4
-#a)
-
+#Question 4a
 xtrainXOR = [[0., 0.], [0., 1.], [1., 0.],[1., 1.]]
-ytrainXOR = [0, 1, 1, 0.]
+ytrainXOR = [0, 1, 1, 0]
+xtestXOR = [[1., 1.], [1., 0.], [0., 1.],[0., 0.]]
+ytestXOR = [0, 1, 1, 0]
+clf = MLPClassifier(hidden_layer_sizes=(), activation='identity',
+                    solver='lbfgs').fit(xtrainXOR, ytrainXOR)
+print("Score obtenu sur les données de test pour l'apprentissage de l'operator XOR sans couche cachée: ", 
+      clf.score(xtestXOR, ytestXOR))
 
-classifier.fit(xtrainXOR, ytrainXOR)
+#Question 4b
+clf_2l = MLPClassifier(hidden_layer_sizes=(4,2), activation='identity',
+                    solver='lbfgs').fit(xtrainXOR, ytrainXOR)
+print("Score obtenu sur les données de test pour l'apprentissage de l'operator XOR avec les 2 couches cachées: ", 
+      clf_2l.score(xtestXOR, ytestXOR))
 
-X_test_XOR = [[1., 1.],[0.,0.],[0.,1.],[1.,0.]]
-Y_test_XOR = [0.,0.,1.,1.]
-Y_predict_XOR = classifier.predict(X_test_XOR)
+#Question 4c
+clf_hyp = MLPClassifier(hidden_layer_sizes=(4,2), activation='tanh',
+                    solver='lbfgs').fit(xtrainXOR, ytrainXOR)
+print("Score obtenu avec la fonction d'activation tanh: ", 
+      clf_hyp.score(xtestXOR, ytestXOR))
 
-classifier.score(X_test_XOR,Y_test_XOR)
-
-#b)
-classifier_XOR_id = MLPClassifier(hidden_layer_sizes=(4,2),activation='identity',
-                           solver='lbfgs')
-
-classifier_XOR_id.fit(xtrainXOR, ytrainXOR)
-Y_predict_XOR_id = classifier_XOR_id.predict(X_test_XOR)
-
-classifier_XOR_id.score(X_test_XOR,Y_test_XOR)
-classifier_XOR_id.coefs_
-
-#c)
-classifier_XOR_tanh = MLPClassifier(hidden_layer_sizes=(4,2),activation='tanh',
-                           solver='lbfgs')
-
-classifier_XOR_tanh.fit(xtrainXOR, ytrainXOR)
-Y_predict_XOR_tanh = classifier_XOR_tanh.predict(X_test_XOR)
-
-classifier_XOR_tanh.score(X_test_XOR,Y_test_XOR)
-classifier_XOR_tanh.coefs_
-
-
-#Question 5 
+#Question 5
 
 dataset = load_digits()
-X = dataset.data 
-Y = dataset.target
+X = dataset.data # Entrees
+Y = dataset.target # Resultats attendus
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1)
-
+clf5 = MLPClassifier(hidden_layer_sizes=(4,2), activation='relu',
+                    solver='lbfgs').fit(X_train, Y_train)
 
 activ = ['identity', 'logistic', 'tanh', 'relu']
 solve = ['lbfgs', 'sgd', 'adam']
@@ -157,42 +120,69 @@ for j in range(len(solve)) :
     for i in range(len(layers)) :
         index.append((solve[j],layers[i]))
 
-a = np.zeros((3,4))
-M = [a,a,a]
+a = []
+M = pd.DataFrame(columns = activ)
 
 for j in range(len(activ)) :
     for i in range(len(solve)) : 
         for k in range(len(layers)) :
-           classifier = MLPClassifier(hidden_layer_sizes=layers[k],
+            classifier = MLPClassifier(hidden_layer_sizes=layers[k],
                                       activation=activ[j],
                                       solver=solve[i])
-           classifier.fit(X_train,Y_train)
-           Y_pred = classifier.predict(X_test)
-           M[k][i,j] = classifier.score(X_test,Y_test)
+            classifier.fit(X_train,Y_train)
+            Y_pred = classifier.predict(X_test)
+            a.append(accuracy_score(Y_test, Y_pred))     
+    M.iloc[:,j] = a
+    a = []
 
 
 classifier = MLPClassifier(hidden_layer_sizes=(),
-                                      activation='identity',
-                                      solver='sgd')
+                                      activation='relu',
+                                      solver='lbfgs')
 classifier.fit(X_train,Y_train)
 Y_pred = classifier.predict(X_test)
 accuracy_score(Y_test, Y_pred)
+classifier.score(X_test,Y_test)
 
+M.columns = activ
+M.index = index
 
-M_results = pd.concat([pd.DataFrame(M[0]), pd.DataFrame(M[1]), 
-                       pd.DataFrame(M[2])])
-M_results.columns = activ
-M_results.index = index
+M.idxmax()
+
 
 
 
 #Question 6 
 
-M_results.idxmax()
-
 #svm
-parameters = {'kernel': ['linear'], 'C': list(np.linspace(0.001, 3, 21))}
+# classifier_svm_poly = SVC(kernel = "poly")
+# classifier_svm_lin = SVC(kernel ="linear")
+
+# scores_lin = cross_val_score(classifier_svm_lin, X_test, Y_test, cv = RepeatedKFold(
+#     n_splits=5, n_repeats=1000))
+# scores_poly = cross_val_score(classifier_svm_poly, X_test, Y_test, cv = RepeatedKFold(
+#     n_splits=5, n_repeats=1000))
+
+# print("Cross-validation: {}",scores_lin.mean())
+# print("Cross-validation: {}",scores_poly.mean())
+
+
+
+parameters = {'kernel': ['linear']}
 model_svm = SVC()
-svm_grid = GridSearchCV(model_svm, parameters, n_jobs=-1, cv = 5)
+svm_grid = GridSearchCV(model_svm, parameters, n_jobs=-1, cv = RepeatedKFold(
+    n_splits=5, n_repeats=100))
 svm_grid.fit(X_train, Y_train)
+print(svm_grid.best_params_)
 print('Score : %s' % svm_grid.score(X_test, Y_test))
+
+
+parameters2 = {'kernel': ['poly']}
+svm_grid2 = GridSearchCV(model_svm, parameters2, n_jobs=-1, cv = RepeatedKFold(
+    n_splits=5, n_repeats=100))
+svm_grid2.fit(X_train, Y_train)
+
+print('Score : %s' % svm_grid2.score(X_test, Y_test))
+
+
+
